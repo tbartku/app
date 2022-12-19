1346a9e942610369220c02d4a8b5be6e69f5e470
tbartku <tbartku@purdue.edu>
2022-12-19 18:21:25 -0500

    initial commit

--- frontend/components/buttonapi/index.js
+++ frontend/components/buttonapi/index.js
@@ -3,20 +3,27 @@ import React from 'react';
 class Welcome extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {number: "0"};
+        this.state = {number: "1"};
+
+        this.setState = this.setState.bind(this);
       }
     
     callapi = () => {
-        fetch('http://127.0.0.1:5000')
+        fetch('http://127.0.0.1:5000');
+        console.log("Call api is running when clicked")
+        
 // fill in code // this.setState({number: })
+      this.state = {data: " "};
+      this.setState = this.setState.bind(this);
 
+     // const {number} = this.state
     }
     
     render() {
         const {number} = this.state
       return (
         <div>
-            <button onClick={callapi}>Alert!</button>
+            <button onClick={this.callapi}>Click to change the number below!</button>
             <h1>
                 {number}
             </h1>
